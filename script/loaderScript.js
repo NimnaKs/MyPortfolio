dbcf797936d28b059cae7e491d985ec2c838006e
NimnaKs <nimnakaveesha123456@gmail.com>
2023-10-28 22:07:46 +0530

    Add Preloader

--- script/loaderScript.js
+++ script/loaderScript.js
@@ -1,9 +1,5 @@
 
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-    // Simulate a delay (3 seconds in this example)
-    setTimeout(function () {
-        document.querySelector("body").style.display = "block";
-        document.querySelector(".loading").style.display = "none";
-    }, 3000); // Adjust the delay in milliseconds
+window.addEventListener("load", () => {
+    console.log("Nimna")
+    $("#preloader").css("display","none");
 });
